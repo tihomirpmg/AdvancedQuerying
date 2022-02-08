49dae55af42f43b8cbaa05ab1eea763e288329a6
Тихомир Найденов <tihomirnaydenov02@pmgkk.com>
2022-02-08 20:37:02 +0200

    Add tasks related with Advanced Querying

---
+++ BookShop/Models/Configurations/AuthorConfig.cs
@@ -0,0 +1,24 @@
+﻿using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookShop.Models.Configurations
+{
+    public class AuthorConfig : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(e => e.AuthorId);
+
+            builder.Property(e => e.FirstName)
+                .IsRequired(false)
+                .IsUnicode()
+                .HasMaxLength(50);
+
+            builder.Property(e => e.LastName)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(50);
+        }
+    }
+}
